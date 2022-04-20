51bbb688d806cc3451d4388066315eb0078e330c
KanarXD <piciolo@outlook.com>
2022-04-20 12:29:07 +0200

    add mpi hello world

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+#include <iostream>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
