cea9405fb62e0634d6ce2f9f4ff9c365a1972513
picio <piciolo@outlook.com>
2022-05-18 19:10:46 +0200

    random using time

--- src/utility/Functions.h
+++ src/utility/Functions.h
@@ -2,20 +2,16 @@
 
 #include <thread>
 #include <random>
-#include <chrono>
-#include <ctime>
 
 #define MIN_SLEEP_TIME 200
 #define MAX_SLEEP_TIME 1000
 
 namespace Functions {
-    static std::mt19937 rng(0);
-
-    static void initRNG() {
-        duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    }
 
     static unsigned long getRandomNumber() {
+        long time = duration_cast<std::chrono::nanoseconds>(
+                std::chrono::system_clock::now().time_since_epoch()).count();
+        std::mt19937 rng(time);
         return rng();
     }
 
@@ -27,7 +23,6 @@ namespace Functions {
     static bool makeDecision(int percentChance) {
         return percentChance > getRandomNumber() % 100;
     }
-
 
 
 }
