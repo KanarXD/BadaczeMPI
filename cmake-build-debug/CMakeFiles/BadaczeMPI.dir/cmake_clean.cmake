46ffde2f503cbaaf9d977d2400a524b5187e288b
KanarXD <piciolo@outlook.com>
2022-06-08 12:05:15 +0200

    c

---
+++ cmake-build-debug/CMakeFiles/BadaczeMPI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BadaczeMPI"
+  "BadaczeMPI.pdb"
+  "CMakeFiles/BadaczeMPI.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BadaczeMPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/src/main.cpp" "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/cmake-build-debug/CMakeFiles/app.dir/src/main.cpp.o"
+  "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/src/models/ProcessData.cpp" "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/cmake-build-debug/CMakeFiles/app.dir/src/models/ProcessData.cpp.o"
+  "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/src/threads/BaseThread.cpp" "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/cmake-build-debug/CMakeFiles/app.dir/src/threads/BaseThread.cpp.o"
+  "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/src/threads/CommunicationThread.cpp" "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/cmake-build-debug/CMakeFiles/app.dir/src/threads/CommunicationThread.cpp.o"
+  "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/src/threads/MainThread.cpp" "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/cmake-build-debug/CMakeFiles/app.dir/src/threads/MainThread.cpp.o"
+  "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/src/utility/Log.cpp" "/mnt/c/Users/picio/CLionProjects/BadaczeMPI/cmake-build-debug/CMakeFiles/app.dir/src/utility/Log.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/x86_64-linux-gnu/mpich"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
